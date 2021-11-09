8f1aa3ec3a3262908520a7762647ae315d4d451b
msftph <65667413+msftph@users.noreply.github.com>
2021-11-08 19:39:59 -0500

    successfully creates logic app

--- terraform/logic-app.tf
+++ terraform/logic-app.tf
@@ -1,9 +1,20 @@
 resource "azurerm_logic_app_standard" "default" {
-    name                = "cmpgitopslogicapp"
+    name                = "cmpgitopslogicapptf"
     location            = azurerm_resource_group.default.location
     resource_group_name = azurerm_resource_group.default.name
   
     app_service_plan_id         = azurerm_app_service_plan.default.id
     storage_account_name        = azurerm_storage_account.default.name
-    storage_account_access_key  = azurerm_storage_account.default.primary_access_key
+    storage_account_access_key  = azurerm_storage_account.default.primary_access_key 
+    storage_account_share_name  = ""    
+    https_only                  = true    
+
+    site_config {
+        vnet_route_all_enabled = "1"
+    }
+
+    app_settings = {
+        "WEBSITE_VNET_ROUTE_ALL" = "1"
+        "WEBSITE_CONTENTOVERVNET" = "1"
+    }
 }
